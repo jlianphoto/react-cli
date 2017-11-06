d41fad3e355b27a6167d9986f770afed951864bf
jlian <872821547@qq.com>
2017-11-07 00:14:21 +0800

    add keybord

--- src/component/pay/index.jsx
+++ src/component/pay/index.jsx
@@ -16,7 +16,7 @@ class Pay extends baseComponent {
 	    return (
 	      <div className="pay">
 	      	<header>
-	      		<img src={'http://p1.music.126.net/2taxduLYm8_vtG2cM89oOg==/1364493943108516.jpg?param=140y140'}/>
+	      		<img alt="商家图片" src={'http://p1.music.126.net/2taxduLYm8_vtG2cM89oOg==/1364493943108516.jpg?param=140y140'}/>
 	      		<h4>海底捞（正佳店）</h4>
 	      	</header>
 	      	<div className="container">
@@ -35,9 +35,36 @@ class Pay extends baseComponent {
 	      			<b>（该商户支持开发票功能）</b>
 	      		</div>
 	      	</div>
-	      	<div>
-	      		键盘
+
+	      	<div className="company">
+	      		dafadsadfgong
 	      	</div>
+
+      		<table className="keybord">
+      			<tbody>
+      			<tr>
+      				<td>1</td>
+      				<td>2</td>
+      				<td>3</td>
+      				<td>icon</td>
+      			</tr>
+      			<tr>
+      				<td>4</td>
+      				<td>5</td>
+      				<td>6</td>
+      				<td rowSpan="3" className="submit">确定付款</td>
+      			</tr>
+      			<tr>
+      				<td>7</td>
+      				<td>8</td>
+      				<td>9</td>
+      			</tr>
+      			<tr>
+      				<td colSpan="2">0</td>
+      				<td>.</td>
+      			</tr>
+      			</tbody>
+      		</table>
 	      </div>
 	    );
 	 }
